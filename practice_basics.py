7f8394a4f23a83cd62f4cc49fb8fa8249a37fadc
lafarris824-wq <lafarris824@gmail.com>
2025-08-26 13:49:57 -0400

    Practice variables, numbers, strings, lists, dicts  if/else, for-loops. Five short exercises.

---
+++ practice_basics.py
@@ -0,0 +1,23 @@
+x=5
+y=10
+print(x + y)
+
+name = 'Lukas'
+print('hello, ' + name)
+
+colors = ['blue', 'green', 'yellow']
+colors.append('red')
+print(colors)
+
+person = {'name': 'Lukas', 'age': 21, 'major': 'MIS'}
+print('Student Major:', person['name'])
+
+numbers = [1, 2, 3, 4, 5]
+for n in numbers:
+    if n % 2 == 0:
+        print(n, 'is even')
+    else:
+        print(n, 'is odd')
+
+
+
